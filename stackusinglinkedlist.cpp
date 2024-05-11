f905e393c5b298a272ef2ad7c0869d1032ac63a8
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-05-11 12:16:29 +0700

    menambahkan perintah untuk mengembalikan value ke fungsi push

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -31,6 +31,7 @@ class stack
             newNode->next = top; // 3. set the next pointer of the new node to the current top node
             top = newNode; // 4. Update the top pointer to the new node
             cout << "Push value: " << value << endl;
+            return value;
         }
 
 };
