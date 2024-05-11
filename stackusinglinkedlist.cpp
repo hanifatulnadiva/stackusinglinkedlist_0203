41aa18ec168f271c5aac3dcbdae453925a39b6ae
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-05-11 12:14:30 +0700

    step 3: membuat medan next pada node baru sama dengan top

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -28,6 +28,7 @@ class stack
         {
             Node* newNode = new Node(); // 1. Allocate memory for the new node
             newNode->data = value; // 2. assign value 
+            newNode->next = top; // 3. set the next pointer of the new node to the current top node
         }
 
 };
